6664b2ea2889344e1e47f1f3e503a6edf66cb05d
sign1337 <lewisnuttyalt@gmail.com>
2026-08-15 06:56:07 +0100

    Add files via upload

---
+++ x64/Release/gooo/pepe - Copy.bat
@@ -0,0 +1,29 @@
+@echo off
+openfiles >nul 2>nul
+if %errorlevel% neq 0 (
+    echo This script is not running as Administrator. Re-launching with admin privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/c ""%~s0""' -Verb runAs"
+    exit /b
+)
+
+pushd "%cd%"
+cd /d "%~dp0"
+
+cd /d "%~dp0"
+echo.
+set "file_path=Sync_protected.dll"
+echo ok, using: "%file_path%"
+echo.
+
+:: === use signtool instead of relapse-signer ===
+"%cd%\signer\signtool\signtool.exe" sign ^
+/f "%cd%\certificates\1.pfx" ^
+/p "ndasec-2016" ^
+/tr http://timestamp.digicert.com ^
+/td sha256 ^
+/fd sha256 ^
+"%file_path%"
+
+echo.
+echo done signing: "%file_path%"
+pause
